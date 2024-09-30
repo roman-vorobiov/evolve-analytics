3f82615e46e48785830fec6a3c6aae135817e1e3
romanv <roman.vorobyov7777@gmail.com>
2024-09-30 02:36:38 +0200

    export as image

--- src/ui/viewTab.ts
+++ src/ui/viewTab.ts
@@ -1,11 +1,64 @@
 import { resets, universes } from "../enums";
-import { weakFor, invokeFor, compose } from "../utils";
+import { weakFor, invokeFor, compose, transformMap } from "../utils";
 import { makeGraph } from "./graph";
 import { makeViewSettings } from "./viewSettings";
 import { makeMilestoneSettings } from "./milestoneSettings";
 import { makeAdditionalInfoSettings } from "./additionalInfoSettings";
 import type { ConfigManager, View } from "../config";
 import type { HistoryEntry, HistoryManager } from "../history";
+
+import type { default as htmltoimage } from "html-to-image";
+
+declare var htmlToImage: typeof htmltoimage;
+
+async function withCSSOverrides<T>(overrides: Record<string, Record<string, string>>, callback: () => Promise<T>): Promise<T> {
+    const overridesList = [];
+    for (const [query, props] of Object.entries(overrides)) {
+        const nodes = $(query);
+        for (const [rule, value] of Object.entries(props)) {
+            for (const node of nodes) {
+                overridesList.push({ node, rule, original: node.style[rule as any], override: value });
+            }
+        }
+    }
+
+    for (const { node, rule, override } of overridesList) {
+        $(node).css(rule, override);
+    }
+
+    const result = await callback();
+
+    for (const { node, rule, original } of overridesList) {
+        $(node).css(rule, original);
+    }
+
+    return result;
+}
+
+async function copyToClipboard(node: JQuery) {
+    const isParent = (element: HTMLElement) => node.closest(element).length !== 0;
+    const isChild = (element: HTMLElement) => $(element).closest(node).length !== 0;
+
+    const cssOverrides = {
+        "html": { width: "1000px", height: "550px" },
+        "#mainColumn": { width: "100%" },
+        ".vscroll": { height: "100%" },
+        ".tab-item": { padding: "0" }
+    };
+
+    const blob = await withCSSOverrides(cssOverrides, () => {
+        return htmlToImage.toBlob($("html")[0], {
+            width: 1000,
+            height: 550,
+            skipFonts: true,
+            filter: element => isParent(element) || isChild(element)
+        });
+    });
+
+    await navigator.clipboard.write([
+        new ClipboardItem({ "image/png": blob! })
+    ]);
+}
 
 function viewTitle(view: View) {
     let title = resets[view.resetType];
@@ -20,27 +73,37 @@ export function makeViewTab(id: string, view: View, config: ConfigManager, histo
     const controlNode = $(`<li><a href="#${id}">${viewTitle(view)}</a></li>`);
     const contentNode = $(`<div id="${id}" class="vscroll" style="height: calc(100vh - 10rem)"></div>`);
 
-    const removeViewNode = $(`<button class="button right" style="margin-right: 1em">Delete View</button>`)
+    const removeViewNode = $(`<button class="button">Delete View</button>`)
         .on("click", () => { config.removeView(view); });
 
     let selectedRun: HistoryEntry | null = null;
 
-    const discardRunNode = $(`<button class="button" style="margin-right: 1em">Discard Run</button>`)
+    const discardRunNode = $(`<button class="button">Discard Run</button>`)
         .on("click", () => { history.discardRun(selectedRun!); })
         .hide();
+
+    const asImageNode = $(`<button class="button">Copy as PNG</button>`)
+        .on("click", async () => {
+            const figure = contentNode.find("> figure");
+            await copyToClipboard(figure);
+        });
 
     function onRunSelection(run: HistoryEntry | null) {
         selectedRun = run;
         discardRunNode.toggle(selectedRun !== null);
     }
 
+    const buttonsContainerNode = $(`<div style="display: flex; justify-content: space-between"></div>`)
+        .append(asImageNode)
+        .append(discardRunNode)
+        .append(removeViewNode);
+
     contentNode
         .append(makeViewSettings(view).css("margin-bottom", "1em"))
         .append(makeAdditionalInfoSettings(view).css("margin-bottom", "1em"))
         .append(makeMilestoneSettings(view).css("margin-bottom", "1em"))
         .append(makeGraph(history, view, onRunSelection))
-        .append(discardRunNode)
-        .append(removeViewNode);
+        .append(buttonsContainerNode);
 
     config.on("viewUpdated", compose([weakFor(view), invokeFor(view)], (updatedView) => {
         controlNode.find("> a").text(viewTitle(updatedView));
