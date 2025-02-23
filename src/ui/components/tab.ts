499c7e28fcf0555cea5992967f13c9d8f72badf3
romanv <roman.vorobyov7777@gmail.com>
2025-02-23 16:01:41 +0100

    vue types

--- src/ui/components/tab.ts
+++ src/ui/components/tab.ts
@@ -1,13 +1,28 @@
 import { waitFor } from "../utils";
 
-type VueBoundElement = HTMLElement & { __vue__: any };
+import type VueType from "vue";
 
-declare const Vue: any;
+declare const Vue: typeof VueType;
+
+type BTabItem = VueType & {
+    index: number | null;
+    $options: {
+        propsData: {
+            label: string
+        }
+    }
+}
+
+type BTabs = VueType & {
+    _registerItem(item: BTabItem): void
+}
+
+type VueBoundElement<T extends VueType> = HTMLElement & { __vue__: T };
 
 export async function addTab(name: string, factory: () => JQuery<HTMLElement>) {
     const tabID = `mTab${name}`;
 
-    const tabs = (await waitFor(`div#mainTabs`) as JQuery<VueBoundElement>)[0].__vue__;
+    const tabs = (await waitFor(`div#mainTabs`) as JQuery<VueBoundElement<BTabs>>)[0].__vue__;
 
     // .sticky messes up tab transitions, replace it with .vscroll
     $("#settings").removeClass("sticky").addClass("vscroll");
@@ -25,16 +40,16 @@ export async function addTab(name: string, factory: () => JQuery<HTMLElement>) {
             // It verifies this by injecting the parent via the btab prop - mock this dependency manually
             return { btab: tabs };
         },
-        mounted() {
+        mounted(this: BTabItem) {
             // Without this, the tabs component doesn't track the state properly
-            tabs.$slots.default.push(this.$children[0].$vnode);
+            tabs.$slots.default!.push(this.$children[0].$vnode);
         }
     });
 
     // For some reason, pushing a vnode to tabs.$slots causes the template to be compiled and mounted twice
     // Ignore consecutive inserts with of the same node
     const original = tabs._registerItem;
-    tabs._registerItem = (item: any) => {
+    tabs._registerItem = (item: BTabItem) => {
         if (item.$options.propsData.label !== name) {
             original(item);
         }
@@ -42,7 +57,7 @@ export async function addTab(name: string, factory: () => JQuery<HTMLElement>) {
 
     // tab.$children[0].index is not initialized yet
     Vue.nextTick(() => {
-        const tabIndex = tab.$children[0].index;
+        const tabIndex = (tab.$children[0] as BTabItem).index;
 
         let initialized = false;
         tabs.$on("input", (idx: number) => {
