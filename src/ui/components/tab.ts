b0f9c48e9aa44305ed773e984de1107c5c307ea0
romanv <roman.vorobyov7777@gmail.com>
2025-02-23 03:14:32 +0100

    comments

--- src/ui/components/tab.ts
+++ src/ui/components/tab.ts
@@ -21,13 +21,18 @@ export async function addTab(name: string, factory: () => JQuery<HTMLElement>) {
     const tab = new Vue({
         el: "#mainTabs > .tab-content > :last-child",
         provide() {
+            // BTabItem requires being compiled inside a BTabs component
+            // It verifies this by injecting the parent via the btab prop - mock this dependency manually
             return { btab: tabs };
         },
         mounted() {
+            // Without this, the tabs component doesn't track the state properly
             tabs.$slots.default.push(this.$children[0].$vnode);
         }
     });
 
+    // For some reason, pushing a vnode to tabs.$slots causes the template to be compiled and mounted twice
+    // Ignore consecutive inserts with of the same node
     const original = tabs._registerItem;
     tabs._registerItem = (item: any) => {
         if (item.$options.propsData.label !== name) {
@@ -35,6 +40,7 @@ export async function addTab(name: string, factory: () => JQuery<HTMLElement>) {
         }
     };
 
+    // tab.$children[0].index is not initialized yet
     Vue.nextTick(() => {
         const tabIndex = tab.$children[0].index;
 
